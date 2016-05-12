c2d2e73bf3d6eb2786ecc5d0f9c9a30e4bdcd9d4
binghaml <binghaml@uw.edu>
2016-05-12 14:09:59 -0700

    FoodItem class and javadocs are written MyFoodItemRecyclerViewAdapter is written, needs some javadocs. FoodItemFragment is started. Still needs work. App is stable. No new functionality is completed but it runs and doesn't crash.

---
+++ app/src/main/java/edu/uw/tacoma/team5/calorit/MyFoodItemRecyclerViewAdapter.java
@@ -0,0 +1,80 @@
+package edu.uw.tacoma.team5.calorit;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import edu.uw.tacoma.team5.calorit.FoodItemFragment.OnListFragmentInteractionListener;
+import edu.uw.tacoma.team5.calorit.model.FoodItem;
+
+
+import java.util.List;
+
+/**
+ * {@link RecyclerView.Adapter} that can display a {@link edu.uw.tacoma.team5.calorit.model.FoodItem} and makes a call to the
+ * specified {@link OnListFragmentInteractionListener}.
+ * TODO: Replace the implementation with code for your data type.
+ */
+public class MyFoodItemRecyclerViewAdapter extends RecyclerView.Adapter<MyFoodItemRecyclerViewAdapter.ViewHolder> {
+
+    private final List<FoodItem> mFoodItems;
+    private final OnListFragmentInteractionListener mListener;
+
+    public MyFoodItemRecyclerViewAdapter(List<FoodItem> items, OnListFragmentInteractionListener listener) {
+        mFoodItems = items;
+        mListener = listener;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_fooditem, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mFoodItems.get(position);
+        holder.mIdView.setText(mFoodItems.get(position).getmFoodName());
+
+        //Maybe don't need this line. Have to work on other parts first to know.
+//        holder.mContentView.setText(mFoodItems.get(position).content);
+
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (null != mListener) {
+                    // Notify the active callbacks interface (the activity, if the
+                    // fragment is attached to one) that an item has been selected.
+                    mListener.onListFragmentInteraction(holder.mItem);
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mFoodItems.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final View mView;
+        public final TextView mIdView;
+        public final TextView mContentView;
+        public FoodItem mItem;
+
+        public ViewHolder(View view) {
+            super(view);
+            mView = view;
+            mIdView = (TextView) view.findViewById(R.id.id);
+            mContentView = (TextView) view.findViewById(R.id.content);
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + mContentView.getText() + "'";
+        }
+    }
+}
---
+++ app/src/main/java/edu/uw/tacoma/team5/calorit/model/FoodItem.java
@@ -0,0 +1,125 @@
+package edu.uw.tacoma.team5.calorit.model;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+
+/**
+ * Created by Levi on 5/12/2016.
+ *
+ * This class is used to model a FoodItem. Instances of this class will represent food items eaten
+ * in a given meal. A meal will be represented as a collection of these FoodItems.
+ */
+public class FoodItem {
+
+    /**
+     * The keys that are used to get food name, calories, and category from JSON
+     */
+    public static final String FOOD_NAME = "foodName", CALORIES = "calories", CATEGORY = "category";
+
+    /**
+     * String representation of the food's name.
+     */
+    private String mFoodName;
+
+    /**
+     * String representation of the food's category (ie. Meat).
+     */
+    private String mCategory;
+
+    /**
+     * Number of calories in 100g of this food.
+     */
+    private int mCalories;
+
+    /**
+     * Constructor for FoodItem
+     * @param mFoodName the name to assign to this FoodItem.
+     * @param mCategory the category that this FoodItem is in.
+     * @param mCalories the number of calories 100g of this FoodItem has.
+     */
+    public FoodItem(String mFoodName, String mCategory, int mCalories) {
+        this.mFoodName = mFoodName;
+        this.mCategory = mCategory;
+        this.mCalories = mCalories;
+    }
+
+    /**
+     * Getter for the FoodItem's name.
+     * @return String representation of the FoodItem's name.
+     */
+    public String getmFoodName() {
+        return mFoodName;
+    }
+
+    /**
+     * Setter for the FoodItem's name.
+     * @param mFoodName String representation that will be assigned to this FoodItem's name.
+     */
+    public void setmFoodName(String mFoodName) {
+        this.mFoodName = mFoodName;
+    }
+
+    /**
+     * Getter for the FoodItem's category.
+     * @return String representation of the FoodItem's category.
+     */
+    public String getmCategory() {
+        return mCategory;
+    }
+
+    /**
+     * Setter for the FoodItem's category.
+     * @param mCategory String representation that will be assigned to the FoodItem's category.
+     */
+    public void setmCategory(String mCategory) {
+        this.mCategory = mCategory;
+    }
+
+    /**
+     * Getter for the number of calories 100g of this FoodItem has.
+     * @return int representing the number of calories in 100g of this FoodItem.
+     */
+    public int getmCalories() {
+        return mCalories;
+    }
+
+    /**
+     * Setter for the number of calories 100g of this FoodItem has.
+     * @param mCalories integer representing how many calories 100g of this FoodItem has.
+     */
+    public void setmCalories(int mCalories) {
+        this.mCalories = mCalories;
+    }
+
+    /**
+     * Gets a list representation of FoodItems from JSON
+     *
+     * @param foodItemJSON JSON representation of the FoodItems
+     * @param foodItems List that each FoodItem in the JSON will be added to.
+     * @return the reason the method failed, if any. NULL = success.
+     */
+    String parseFoodItemJSON(String foodItemJSON, List<FoodItem> foodItems){
+        String reason = null;
+
+        if (foodItemJSON != null) {
+            try {
+                JSONArray arr = new JSONArray(foodItemJSON);
+
+                for (int i = 0; i < arr.length(); i++) {
+                    JSONObject obj = arr.getJSONObject(i);
+
+                    FoodItem foodItem = new FoodItem(obj.getString(FoodItem.FOOD_NAME), obj.getString(FoodItem.CATEGORY),
+                            Integer.valueOf(obj.getString(FoodItem.CALORIES)));
+                    foodItems.add(foodItem);
+                }
+            } catch (JSONException e) {
+                reason =  "Unable to parse data, Reason: " + e.getMessage();
+            }
+        }
+        return reason;
+    }
+
+}
